485f12eef16df291ff3e2ecb66edd04041b9e0fa
juaamol <jambulud@gmail.com>
2023-07-11 22:08:56 +0200

    basic validation budget

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const budget = ref(0);
+const error = ref('');
+
+const emit = defineEmits(['define-budget']);
+
+const defineBudget = () => {
+  if (budget.value <= 0 || typeof budget.value !== 'number') {
+  }
+};
+</script>
 
 <template>
-  <form>
+  <form class="budget" @submit.prevent="defineBudget">
     <div class="field">
       <label for="new-budget">Define budget</label>
       <input
@@ -9,10 +21,49 @@
         id="new-budget"
         class="new-budget"
         placeholder="Add your budget"
+        min="0"
+        v-model.number="budget"
       />
     </div>
     <button type="submit">Define your budget</button>
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.budget {
+  width: 100%;
+}
+.field {
+  display: grid;
+  gap: 2rem;
+}
+.budget label {
+  font-size: 2.2rem;
+  text-align: center;
+  color: var(--blue);
+}
+.budget input[type='number'] {
+  background-color: var(--light-gray);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+  text-align: center;
+}
+.budget button[type='submit'] {
+  background-color: var(--blue);
+  border: none;
+  padding: 1rem;
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 2rem;
+  color: var(--white);
+  font-weight: 900;
+  width: 100%;
+  transition: background-color 300ms ease;
+}
+.budget button[type='submit']:hover {
+  background-color: #1048a4;
+  cursor: pointer;
+}
+</style>
