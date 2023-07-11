277edbd90d452d169db3ab7e273c8c1a8c948a63
juaamol <jambulud@gmail.com>
2023-07-11 22:00:29 +0200

    global styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,81 @@
 <script setup>
-import Filters from './components/Filters.vue';
+import Budget from './components/Budget.vue';
 </script>
 
 <template>
   <div>
-    <header>Expenses Manager</header>
-    <Filters />
+    <header>
+      <h1>Expenses Manager</h1>
+
+      <div class="container-header container shadow">
+        <Budget />
+      </div>
+    </header>
   </div>
 </template>
 
-<style></style>
+<style>
+:root {
+  --blue: #3b82f6;
+  --white: #fff;
+  --light-gray: #f5f5f5;
+  --gray: #94a3b8;
+  --dark-gray: #64748b;
+  --black: #000;
+}
+
+html {
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: 'Lato', sans-serif;
+  background-color: var(--light-gray);
+}
+
+h1 {
+  font-size: 4rem;
+}
+
+h2 {
+  font-size: 3rem;
+}
+
+header {
+  background-color: var(--blue);
+}
+
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--white);
+  text-align: center;
+}
+
+.container-header {
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+}
+
+.container {
+  margin-top: -5rem;
+  transform: translateY(5rem);
+  padding: 5rem;
+}
+
+.shadow {
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  background-color: var(--white);
+  border-radius: 1.2rem;
+  padding: 5rem;
+}
+</style>
---
+++ src/components/Budget.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <form>
+    <div class="field">
+      <label for="new-budget">Define budget</label>
+      <input
+        type="number"
+        id="new-budget"
+        class="new-budget"
+        placeholder="Add your budget"
+      />
+    </div>
+    <button type="submit">Define your budget</button>
+  </form>
+</template>
+
+<style scoped></style>
